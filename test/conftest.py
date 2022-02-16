f7c6e5a25c3f4c0752017e0318677dcc98963655
mykhailo <mykha@mykhailos-MacBook-Pro.local>
2022-02-16 13:14:00 +0200

    config.py has been added

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,25 @@ import pytest
 
 from selenium import webdriver
 
+from src.config import Config
+
+
+def pytest_addoption(parser):
+    parser.addoption('--env',
+                     action='store',
+                     default='dev',
+                     help='Environment to run tests.')
+
+
+@pytest.fixture(scope='session')
+def env(request):
+    return request.config.getoption('--env')
+
+
+@pytest.fixture(scope='session')
+def env_config(env):
+    return Config(env)
+
 
 @pytest.fixture(scope='session')
 def driver():

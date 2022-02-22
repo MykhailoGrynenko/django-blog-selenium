473834be91c4ae02e1a3837bda9237583cb4e312
mykhailo <mykha@mykhailos-MacBook-Pro.local>
2022-02-22 17:38:38 +0200

    Better assertions, removed duplicated test.

--- test/test_login.py
+++ test/test_login.py
@@ -1,14 +1,14 @@
 import os
 
 from src.page import LoginPage
-from src.locators import NavigationLocators
+from src.locators import NavigationLocators, LoginLocators
 
 
 def test_login_invalid_credentials(driver, env_config):
-    LoginPage(env_config, driver).open.login_as('incorrect_username',
-                                                'incorrect_password')
-    assert 'Please enter a correct username and password.'\
-           in driver.page_source
+    login_page = LoginPage(env_config, driver).open
+    login_page.login_as('incorrect_username', 'incorrect_password')
+    assert 'Please enter a correct username and password.' \
+           in login_page.find_element(LoginLocators.incorrect_data).text
 
 
 def test_login_valid_credentials(driver, env_config):
--- test/test_register.py
+++ test/test_register.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.page import RegisterPage
+from src.locators import RegisterLocators
 
 
 @pytest.mark.parametrize('email', ['', 'abc', '@mail.com' 'gns@', 'kek@com', 'dot@edu.'])
@@ -16,15 +17,17 @@ def test_register_invalid_email(email, driver, env_config, fake):
 def test_register_invalid_only_numeric_password(password, driver, env_config, fake):
     register_page = RegisterPage(env_config, driver).open
     register_page.register_as(fake.user_name(), fake.email(), password, password)
-    assert 'This password is entirely numeric.' in driver.page_source
-    assert "Your account has been created. You are now able to log in!" not in driver.page_source
+    assert 'This password is entirely numeric.'\
+           in register_page.find_element(RegisterLocators.error_password).text
+    assert "Your account has been created. You are now able to log in!" not in driver.page_source#
 
 
 @pytest.mark.parametrize('password', ['123456a', 'shorpas', 'a'])
 def test_register_invalid_short_password(password, driver, env_config, fake):
     register_page = RegisterPage(env_config, driver).open
     register_page.register_as(fake.user_name(), fake.email(), password, password)
-    assert 'This password is too short.' in driver.page_source
+    assert 'This password is too short.'\
+           in register_page.find_element(RegisterLocators.error_password).text
     assert "Your account has been created. You are now able to log in!" not in driver.page_source
 
 
@@ -32,15 +35,8 @@ def test_register_invalid_short_password(password, driver, env_config, fake):
 def test_register_invalid_common_password(password, driver, env_config, fake):
     register_page = RegisterPage(env_config, driver).open
     register_page.register_as(fake.user_name(), fake.email(), password, password)
-    assert 'This password is too common.' in driver.page_source
-    assert "Your account has been created. You are now able to log in!" not in driver.page_source
-
-
-@pytest.mark.parametrize('password', ['123', '124', '646464', '3647845678'])
-def test_register_invalid_only_numeric_password(password, driver, env_config, fake):
-    register_page = RegisterPage(env_config, driver).open
-    register_page.register_as(fake.user_name(), fake.email(), password, password)
-    assert 'This password is entirely numeric.' in driver.page_source
+    assert 'This password is too common.' \
+           in register_page.find_element(RegisterLocators.error_password).text
     assert "Your account has been created. You are now able to log in!" not in driver.page_source
 
 
@@ -49,7 +45,8 @@ def test_register_passwords_do_not_match(driver, env_config, fake):
     fake_password2 = fake_password + '_'
     register_page = RegisterPage(env_config, driver).open
     register_page.register_as(fake.user_name(), fake.email(), fake_password, fake_password2)
-    assert "The two password fields didn’t match." in driver.page_source
+    assert "The two password fields didn’t match." \
+           in register_page.find_element(RegisterLocators.error_password).text
     assert "Your account has been created. You are now able to log in!" not in driver.page_source
 
 
@@ -57,4 +54,5 @@ def test_register_valid_credentials(driver, env_config, fake):
     fake_password = fake.password()
     register_page = RegisterPage(env_config, driver).open
     register_page.register_as(fake.user_name(), fake.email(), fake_password, fake_password)
-    assert 'Your account has been created. You are now able to log in!' in driver.page_source
+    assert 'Your account has been created. You are now able to log in!'\
+           in register_page.find_element(RegisterLocators.valid_registration).text

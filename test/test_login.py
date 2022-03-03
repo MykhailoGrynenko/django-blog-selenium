f0ea43e6d2f9a91a82e342e17828692a2774858f
mykhailo <mykha@mykhailos-MacBook-Pro.local>
2022-03-03 18:21:31 +0200

    allure and xdist have been added

--- test/test_login.py
+++ test/test_login.py
@@ -5,7 +5,7 @@ from src.page import LoginPage
 from src.locators import NavigationLocators, LoginLocators
 
 
-@allure.step("don't know what's happening - ")
+@allure.step
 def test_login_invalid_credentials(driver, env_config):
     login_page = LoginPage(env_config, driver).open
     login_page.login_as('incorrect_username', 'incorrect_password')
@@ -16,7 +16,6 @@ def test_login_invalid_credentials(driver, env_config):
 @allure.step
 def test_login_valid_credentials(driver, env_config):
     login_page = LoginPage(env_config, driver).open
-    login_page.login_as(os.environ.get('DJANGO_USER'),
-                        os.environ.get('DJANGO_PASS'))
+    login_page.login_as('TestUser33', 'qwerty54321')
     assert login_page.is_displayed(NavigationLocators.new_post)
     login_page.logout()
--- test/test_register.py
+++ test/test_register.py
@@ -1,81 +1,81 @@
-# import pytest
-# import allure
-#
-# from src.page import RegisterPage
-# from src.locators import RegisterLocators
-#
-#
-# @allure.step
-# @pytest.mark.parametrize('email', ['', 'abc', '@mail.com' 'gns@', 'kek@com', 'dot@edu.'])
-# def test_register_invalid_email(email, driver, env_config, fake):
-#     fake_password = fake.password()
-#     register_page = RegisterPage(env_config, driver).open
-#     register_page.register_as(fake.user_name(), email, fake_password, fake_password)
-#     assert 'Enter a valid email address.' or 'Join Today' in driver.page_source
-#     assert "Your account has been created. You are now able to log in!" not in driver.page_source
-#
-#
-# @allure.step('numeric password tests')
-# @pytest.mark.parametrize('password', ['123', '124', '646464', '3647845678'])
-# def test_register_invalid_only_numeric_password(password, driver, env_config, fake):
-#     register_page = RegisterPage(env_config, driver).open
-#     register_page.register_as(fake.user_name(), fake.email(), password, password)
-#     assert 'This password is entirely numeric.'\
-#            in register_page.find_element(RegisterLocators.error_password).text
-#     assert "Your account has been created. You are now able to log in!" not in driver.page_source#
-#
-#
-# @allure.step
-# @pytest.mark.parametrize('password', ['123456a', 'shorpas', 'a'])
-# def test_register_invalid_short_password(password, driver, env_config, fake):
-#     register_page = RegisterPage(env_config, driver).open
-#     register_page.register_as(fake.user_name(), fake.email(), password, password)
-#     assert 'This password is too short.'\
-#            in register_page.find_element(RegisterLocators.error_password).text
-#     assert "Your account has been created. You are now able to log in!" not in driver.page_source
-#
-#
-# @allure.step
-# @pytest.mark.parametrize('password', ['qwerty123', 'abcdefgh', 'password', 'testing1'])
-# def test_register_invalid_common_password(password, driver, env_config, fake):
-#     register_page = RegisterPage(env_config, driver).open
-#     register_page.register_as(fake.user_name(), fake.email(), password, password)
-#     assert 'This password is too common.' \
-#            in register_page.find_element(RegisterLocators.error_password).text
-#     assert "Your account has been created. You are now able to log in!" not in driver.page_source
-#
-#
-# @allure.step
-# def test_register_passwords_do_not_match(driver, env_config, fake):
-#     fake_password = fake.password()
-#     fake_password2 = fake_password + '_'
-#     register_page = RegisterPage(env_config, driver).open
-#     register_page.register_as(fake.user_name(), fake.email(), fake_password, fake_password2)
-#     assert "The two password fields didn’t match." \
-#            in register_page.find_element(RegisterLocators.error_password).text
-#     assert "Your account has been created. You are now able to log in!" not in driver.page_source
-#
-#
-# @allure.step
-# def test_register_valid_credentials(driver, env_config, fake):
-#     fake_password = fake.password()
-#     register_page = RegisterPage(env_config, driver).open
-#     register_page.register_as(fake.user_name(), fake.email(), fake_password, fake_password)
-#     assert 'Your account has been created. You are now able to log in!'\
-#            in register_page.find_element(RegisterLocators.valid_registration).text
-#
-#
-# @allure.step
-# def test_register_username_already_exists(driver, env_config, fake, register):
-#     register_page = RegisterPage(env_config, driver).open
-#     register_page.register_as(register[0], fake.email(), register[2], register[2])
-#     assert 'A user with that username already exists.'\
-#            in register_page.find_element(RegisterLocators.error_username).text
-#
-#
-# @allure.step
-# @pytest.mark.xfail(reason='There is a bug, therefore, the test fails.')
-# def test_register_email_already_exists(driver, env_config, fake, register):
-#     register_page = RegisterPage(env_config, driver).open
-#     register_page.register_as(fake.user_name(), register[1], register[2], register[2])
-#     assert 'A user with that email already exists.' in driver.page_source
+import pytest
+import allure
+
+from src.page import RegisterPage
+from src.locators import RegisterLocators
+
+
+@allure.step
+@pytest.mark.parametrize('email', ['', 'abc', '@mail.com' 'gns@', 'kek@com', 'dot@edu.'])
+def test_register_invalid_email(email, driver, env_config, fake):
+    fake_password = fake.password()
+    register_page = RegisterPage(env_config, driver).open
+    register_page.register_as(fake.user_name(), email, fake_password, fake_password)
+    assert 'Enter a valid email address.' or 'Join Today' in driver.page_source
+    assert "Your account has been created. You are now able to log in!" not in driver.page_source
+
+
+@allure.step('numeric password tests')
+@pytest.mark.parametrize('password', ['123', '124', '646464', '3647845678'])
+def test_register_invalid_only_numeric_password(password, driver, env_config, fake):
+    register_page = RegisterPage(env_config, driver).open
+    register_page.register_as(fake.user_name(), fake.email(), password, password)
+    assert 'This password is entirely numeric.'\
+           in register_page.find_element(RegisterLocators.error_password).text
+    assert "Your account has been created. You are now able to log in!" not in driver.page_source#
+
+
+@allure.step
+@pytest.mark.parametrize('password', ['123456a', 'shorpas', 'a'])
+def test_register_invalid_short_password(password, driver, env_config, fake):
+    register_page = RegisterPage(env_config, driver).open
+    register_page.register_as(fake.user_name(), fake.email(), password, password)
+    assert 'This password is too short.'\
+           in register_page.find_element(RegisterLocators.error_password).text
+    assert "Your account has been created. You are now able to log in!" not in driver.page_source
+
+
+@allure.step
+@pytest.mark.parametrize('password', ['qwerty123', 'abcdefgh', 'password', 'testing1'])
+def test_register_invalid_common_password(password, driver, env_config, fake):
+    register_page = RegisterPage(env_config, driver).open
+    register_page.register_as(fake.user_name(), fake.email(), password, password)
+    assert 'This password is too common.' \
+           in register_page.find_element(RegisterLocators.error_password).text
+    assert "Your account has been created. You are now able to log in!" not in driver.page_source
+
+
+@allure.step
+def test_register_passwords_do_not_match(driver, env_config, fake):
+    fake_password = fake.password()
+    fake_password2 = fake_password + '_'
+    register_page = RegisterPage(env_config, driver).open
+    register_page.register_as(fake.user_name(), fake.email(), fake_password, fake_password2)
+    assert "The two password fields didn’t match." \
+           in register_page.find_element(RegisterLocators.error_password).text
+    assert "Your account has been created. You are now able to log in!" not in driver.page_source
+
+
+@allure.step
+def test_register_valid_credentials(driver, env_config, fake):
+    fake_password = fake.password()
+    register_page = RegisterPage(env_config, driver).open
+    register_page.register_as(fake.user_name(), fake.email(), fake_password, fake_password)
+    assert 'Your account has been created. You are now able to log in!'\
+           in register_page.find_element(RegisterLocators.valid_registration).text
+
+
+@allure.step
+def test_register_username_already_exists(driver, env_config, fake, register):
+    register_page = RegisterPage(env_config, driver).open
+    register_page.register_as(register[0], fake.email(), register[2], register[2])
+    assert 'A user with that username already exists.'\
+           in register_page.find_element(RegisterLocators.error_username).text
+
+
+@allure.step
+@pytest.mark.xfail(reason='There is a bug, therefore, the test fails.')
+def test_register_email_already_exists(driver, env_config, fake, register):
+    register_page = RegisterPage(env_config, driver).open
+    register_page.register_as(fake.user_name(), register[1], register[2], register[2])
+    assert 'A user with that email already exists.' in driver.page_source
